e15179f14f9ef4c3a3681fb78130ea1fe795adcb
Ben Cavins <ben.cavins@flatironschool.com>
2024-02-06 10:51:05 -0500

    do mock

--- server/app.py
+++ server/app.py
@@ -25,6 +25,77 @@ db.init_app(app)
 def home():
     return ''
 
+@app.route('/scientists', methods=['GET', 'POST'])
+def all_scientists():
+    if request.method == 'GET':
+        scientists = Scientist.query.all()
+        return [s.to_dict(rules=['-missions']) for s in scientists], 200
+    elif request.method == 'POST':
+        json_data = request.get_json()
+
+        try:
+            new_sci = Scientist(
+                name=json_data.get('name'),
+                field_of_study=json_data.get('field_of_study')
+            )
+        except ValueError as e:
+            return {'error': str(e)}, 400
+
+        print(new_sci.id)  # None
+        db.session.add(new_sci)
+        db.session.commit()
+        print(new_sci.id)  # 6 (or whatever the next id is)
+
+        return new_sci.to_dict(rules=['-missions']), 201
+
+@app.route('/scientists/<int:id>', methods=['GET', 'PATCH', 'DELETE'])
+def scientist_by_id(id):
+    scientist = Scientist.query.filter(Scientist.id == id).first()
+
+    if scientist is None:
+        return {'error': 'scientist not found'}, 404
+
+    if request.method == 'GET':
+        return scientist.to_dict(), 200
+    elif request.method == 'PATCH':
+        json_data = request.get_json()
+
+        try:
+            for field in json_data:
+                setattr(scientist, field, json_data[field])
+        except ValueError as e:
+            return {'error': str(e)}, 400
+        
+        db.session.add(scientist)
+        db.session.commit()
+
+        return scientist.to_dict(rules=['-missions']), 202
+    elif request.method == 'DELETE':
+        db.session.delete(scientist)
+        db.session.commit()
+        return {}, 204
+    
+@app.route('/planets')
+def all_planets():
+    planets = Planet.query.all()
+    return [p.to_dict(rules=['-missions']) for p in planets], 200
+
+@app.route('/missions', methods=['POST'])
+def all_missions():
+    json_data = request.get_json()
+    try:
+        new_mission = Mission(
+            name=json_data.get('name'),
+            scientist_id=json_data.get('scientist_id'),
+            planet_id=json_data.get('planet_id')
+        )
+    except ValueError as e:
+        return {'error': str(e)}, 400
+    
+    db.session.add(new_mission)
+    db.session.commit()
+    return new_mission.to_dict(), 201
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -22,10 +22,18 @@ class Planet(db.Model, SerializerMixin):
 
     # Add database columns
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    distance_from_earth = db.Column(db.Integer)
+    nearest_star = db.Column(db.String)
 
     # Add relationship
+    missions = db.relationship('Mission', back_populates='planet', cascade='all, delete-orphan')
 
     # Add serialization rules
+    serialize_rules = ['-missions.planet']
+
+    def __repr__(self) -> str:
+        return f"<Planet {self.name}>"
 
 
 class Scientist(db.Model, SerializerMixin):
@@ -33,10 +41,29 @@ class Scientist(db.Model, SerializerMixin):
 
     # Add database columns
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    field_of_study = db.Column(db.String)
 
     # Add relationship
+    missions = db.relationship(
+        'Mission', 
+        back_populates='scientist',
+        cascade='all, delete-orphan'  # delete the mission if the scientist is deleted
+    )
 
     # Add serialization rules
+    serialize_rules = ['-missions.scientist']
+
+    @validates('name', 'field_of_study')
+    def validate_name_fos(self, key, new_value):
+        if new_value is None:
+            raise ValueError(f'{key} cannot be null')
+        if len(new_value) == 0:
+            raise ValueError(f'{key} must have at least one char')
+        return new_value
+
+    def __repr__(self) -> str:
+        return f"<Scientist {self.name}>"
 
 
 class Mission(db.Model, SerializerMixin):
@@ -44,7 +71,22 @@ class Mission(db.Model, SerializerMixin):
 
     # Add database columns
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    scientist_id = db.Column(db.Integer, db.ForeignKey('scientists.id'))
+    planet_id = db.Column(db.Integer, db.ForeignKey('planets.id'))
 
     # Add relationships
+    scientist = db.relationship('Scientist', back_populates='missions')
+    planet = db.relationship('Planet', back_populates='missions')
 
     # Add serialization rules
+    serialize_rules = ['-scientist.missions', '-planet.missions']
+
+    @validates('name', 'scientist_id', 'planet_id')
+    def validate_fields(self, key, new_value):
+        if new_value is None:
+            raise ValueError(f'{key} cannot be null')
+        return new_value
+
+    def __repr__(self) -> str:
+        return f"<Mission {self.name} {self.scientist_id} {self.planet_id}>"
