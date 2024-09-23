9786f46b0d5179b7ca548d670206f286d07c625b
Ben Cavins <ben.cavins@flatironschool.com>
2024-09-23 15:13:47 -0400

    do lab pt 1

--- server/models.py
+++ server/models.py
@@ -26,35 +26,72 @@ class Planet(db.Model, SerializerMixin):
     nearest_star = db.Column(db.String)
 
     # Add relationship
+    missions = db.relationship('Mission', back_populates='planet')
 
     # Add serialization rules
+    serialize_rules = ['-missions.planet']
+
+    def __repr__(self) -> str:
+        return f'<Planet {self.name} {self.distance_from_earth} {self.nearest_star}>'
 
 
 class Scientist(db.Model, SerializerMixin):
     __tablename__ = 'scientists'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    field_of_study = db.Column(db.String)
+    name = db.Column(db.String, nullable=False)
+    field_of_study = db.Column(db.String, nullable=False)
 
     # Add relationship
+    missions = db.relationship('Mission', back_populates='scientist')
 
     # Add serialization rules
+    serialize_rules = ['-missions.scientist']
 
     # Add validation
+    @validates('name', 'field_of_study')
+    def validate_not_null(self, key, new_value):
+        if not new_value:
+            raise ValueError(f'{key} cannot be empty')
+        else:
+            return new_value
+
+    def __repr__(self) -> str:
+        return f'<Scientist {self.name} {self.field_of_study}>'
 
 
 class Mission(db.Model, SerializerMixin):
     __tablename__ = 'missions'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String, nullable=False)
+    scientist_id = db.Column(db.Integer, db.ForeignKey('scientists.id'), nullable=False)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planets.id'), nullable=False)
 
     # Add relationships
+    scientist = db.relationship('Scientist', back_populates='missions')
+    planet = db.relationship('Planet', back_populates='missions')
 
     # Add serialization rules
+    serialize_rules = ['-scientist.missions', '-planet.missions']
 
     # Add validation
+    @validates('scientist_id', 'planet_id')
+    def validate_not_null(self, key, new_value):
+        if new_value is None:
+            raise ValueError(f'{key} cannot be empty')
+        else:
+            return new_value
+        
+    @validates('name')
+    def validate_not_empty_string(self, key, new_value):
+        if new_value is None or len(new_value) == 0:
+            raise ValueError(f'{key} cannot be empty')
+        else:
+            return new_value
+
+    def __repr__(self) -> str:
+        return f'<Mission {self.name} {self.scientist_id} {self.planet_id}>'
 
 
 # add any models you may need.
